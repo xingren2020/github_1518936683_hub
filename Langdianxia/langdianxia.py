267709a8a7b3ac5f021a5204e631554692708fb3
1518936683 <75053570+1518936683@users.noreply.github.com>
2020-11-26 13:26:42 +0800

    Create langdianxia.py

---
+++ Langdianxia/langdianxia.py
@@ -0,0 +1,50 @@
+import requests
+import os
+import re
+import json
+def Av(i,hd,j,k):
+    print('=🔔='*k)
+    try:
+       response = requests.post(i,headers=eval(hd),data=j)
+       if(k==1):
+       	     print(json.loads(response.text)['data']['score'])
+       elif(k==2):
+       	     print(json.loads(response.text)['data']['score'])
+    except Exception as e:
+      print(str(e))
+
+
+def watch(flag,list):
+   vip=''
+   if flag in osenviron:
+      vip = osenviron[flag]
+   if vip:
+       for line in vip.split('\n'):
+         if not line:
+            continue 
+         list.append(line.strip())
+       return list
+   else:
+       print(f'''【{flag}】 is empty,DTask is over.''')
+       exit()
+       
+
+
+
+
+def start():
+   urllist=[]
+   hdlist=[]
+   bdlist=[]
+   watch('sam_url',urllist)
+   watch('sam_headers',hdlist)
+   watch('sam_body0',bdlist)
+   watch('sam_body1',bdlist)
+   for j in range(1):
+       print('===='+str(j))
+       for k in range(2):
+           Av(urllist[k],hdlist[0],bdlist[k+j],(k+1))
+   print('🔔'*15)
+if __name__ == '__main__':
+       start()
+    
