5f01badfeb8224cf5eea3baa7561501db004269e
1518936683 <75053570+1518936683@users.noreply.github.com>
2020-12-15 19:12:05 +0800

    Delete story_update.py

--- Story/story_update.py
+++
@@ -1,9 +0,0 @@
-def Date_Write(msg,id,ct):
-   with open(str(id)+ct+'.txt','a',encoding='utf-8') as f:
-        f.write(msg)
-        
-        
-        
-        
-        
-Date_Write('5556','222','zoom')
